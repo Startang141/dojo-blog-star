5dcd89e6fcc65a3404eac46ec4d87c3153c1788e
startang141m <bintangadiyatma141@gmail.com>
2023-08-18 14:08:12 +0700

    useState Hook

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Home = () => {
     const handleClick = (e) => {
         console.log('hello bang', e);
@@ -5,6 +7,16 @@ const Home = () => {
 
     const handleClickAgain = (name) => {
         console.log('hello'+ name);
+    }   
+
+
+    // Menggunakan UseState Untuk reactive
+    const [nama, setNama] = useState('BINTANG');
+    const [umur, setUmur] = useState(16);
+    
+    const ClickChange = () => {
+        setNama('WAWAWN');
+        setUmur(99);
     }
 
     return ( 
@@ -12,6 +24,8 @@ const Home = () => {
             <h2>HomePage</h2>
             <button onClick={handleClick}>Click Me</button>
             <button onClick={() => handleClickAgain('wawan')}>Click Me Again</button>
+            <p>{ nama } SUDAH HIDUP { umur }</p>
+            <button onClick={ClickChange}>Change Me</button>
         </div>
      );
 }
