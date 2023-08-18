a6d0147a11eadfc956826b70f9ec68a435304e4e
startang141m <bintangadiyatma141@gmail.com>
2023-08-18 13:49:56 +0700

    Click Events

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('hello bang', e);
+    }
+
+    const handleClickAgain = (name) => {
+        console.log('hello'+ name);
+    }
+
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={() => handleClickAgain('wawan')}>Click Me Again</button>
         </div>
      );
 }
