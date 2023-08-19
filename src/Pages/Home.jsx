75b2e76c0f1cc9c8062014500d7f6ed82a9fe7aa
startang141m <bintangadiyatma141@gmail.com>
2023-08-19 21:08:18 +0700

    Error Handler

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,20 +2,32 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([])
+    const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [isError, setIsError] = useState(null)
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
         .then(res => {
+            if(!res.ok){
+                throw Error('Could Not Fetch Data')
+            }
             return res.json();
         }).then((data)=> {
-            setBlogs(data)
+            setBlogs(data);
+            setIsPending(false);
+            setIsError(null);
+        }).catch((err)=> {
+            setIsPending(false);
+            setIsError(err.message)
         })
     }, [])
 
     return ( 
         <div className="home">
-            {<BlogList blogs={blogs} title='All Blogs!'/>}
+            {isError && <div> {isError} </div>}
+            {isPending && <div>LOADING....</div>}
+            {blogs && <BlogList blogs={blogs} title='All Blogs!'/>}
         </div>
      );
 }
