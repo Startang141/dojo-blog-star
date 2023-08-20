3152ba93377d175513f6e1611d4ed68b47ce6311
startang141m <bintangadiyatma141@gmail.com>
2023-08-20 13:08:09 +0700

    exact match route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 
 import Navbar from './Pages/Navbar';
 import Home from './Pages/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Pages/Create';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <Navbar />
       <div className="content">
-        <Home />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/Create">
+            <Create />
+          </Route>
+        </Switch>
       </div>
     </div>
+    </Router>
   );
 }
 
