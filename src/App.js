4e2ba27cab844b34b9eb237a0b72e10193560b11
startang141m <bintangadiyatma141@gmail.com>
2023-08-20 14:18:59 +0700

    Route Params

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-
 import Navbar from './Pages/Navbar';
 import Home from './Pages/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './Pages/Create';
+import BlogDetail from './Pages/BlogDetail';
 
 function App() {
   return (
@@ -16,6 +16,9 @@ function App() {
           </Route>
           <Route path="/Create">
             <Create />
+          </Route>
+          <Route path="/blogs/:id">
+            <BlogDetail />
           </Route>
         </Switch>
       </div>
